8380c1f2bbe9057ab9db2e116f0d2d3f26facd81
居玉皓 <heiwu805@hotmail.com>
2015-12-08 11:17:53 +0800

    fetch single movie

--- index.ios.js
+++ index.ios.js
@@ -13,36 +13,13 @@ var {
     View,
     } = React;
 
-var MOCKED_MOVIES_DATA = [
-    {title: 'my Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
-
-var AwesomeProject = React.createClass({
-    render: function () {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                Welcome to React Native hehe!
-                </Text>
-                <Text style={styles.instructions}>
-                To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                Press Cmd+R to reload,{'\n'}
-                Cmd+D or shake for dev menu
-                </Text>
-            </View>
-        );
-    }
-});
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
@@ -52,6 +29,83 @@ var styles = StyleSheet.create({
         textAlign: 'center',
         color: '#333333',
         marginBottom: 5,
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
+    },
+    thumbnail: {
+        width: 53,
+        height: 81,
+    },
+});
+
+
+var AwesomeProject = React.createClass({
+    getInitialState: function() {
+        return {
+            movies: null,
+        };
+    },
+    componentDidMount: function() {
+        this.fetchData();
+    },
+    fetchData: function() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies,
+                });
+            })
+            .done();
+    },
+    render: function() {
+        if (!this.state.movies) {
+            return this.renderLoadingView();
+        }
+
+        var movie = this.state.movies[0];
+        return this.renderMovie(movie);
+    },
+
+    renderLoadingView: function() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                Loading movies...
+                </Text>
+            </View>
+        );
+    },
+
+    renderMovie: function(movie) {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );
     },
 });
 
