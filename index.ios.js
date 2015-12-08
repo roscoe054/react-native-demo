dbb42cbfc62334b38f404b4f37fd0d2d411adeb7
居玉皓 <heiwu805@hotmail.com>
2015-12-08 10:52:46 +0800

    init

---
+++ index.ios.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+    AppRegistry,
+    Image,
+    StyleSheet,
+    Text,
+    View,
+    } = React;
+
+var MOCKED_MOVIES_DATA = [
+    {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var AwesomeProject = React.createClass({
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                Welcome to React Native hehe!
+                </Text>
+                <Text style={styles.instructions}>
+                To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                Press Cmd+R to reload,{'\n'}
+                Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
