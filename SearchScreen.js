a734ae15aff17a5b4cf6e7b719c888510bf0e212
居玉皓 <heiwu805@hotmail.com>
2015-12-11 15:18:20 +0800

    moive ex init

---
+++ SearchScreen.js
@@ -0,0 +1,90 @@
+/**
+ * The examples provided by Facebook are for non-commercial testing and
+ * evaluation purposes only.
+ *
+ * Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @flow
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  ActivityIndicatorIOS,
+  ListView,
+  Platform,
+  ProgressBarAndroid,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var TimerMixin = require('react-timer-mixin');
+var invariant = require('invariant');
+var dismissKeyboard = require('dismissKeyboard');
+
+var SearchBar = require('SearchBar');
+
+var SearchScreen = React.createClass({
+  mixins: [TimerMixin],
+  timeoutID: (null: any),
+
+  getInitialState: function() {
+    return {
+      isLoading: false,
+      isLoadingTail: false,
+      filter: '',
+      queryNumber: 0,
+    };
+  },
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text>SearchBar</Text>
+        <SearchBar
+            onSearchChange={this.onSearchChange}
+            isLoading={this.state.isLoading}
+        />
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  centerText: {
+    alignItems: 'center',
+  },
+  noMoviesText: {
+    marginTop: 80,
+    color: '#888888',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#eeeeee',
+  },
+  scrollSpinner: {
+    marginVertical: 20,
+  },
+  rowSeparator: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    height: 1,
+    marginLeft: 4,
+  },
+  rowSeparatorHide: {
+    opacity: 0.0,
+  },
+});
+
+module.exports = SearchScreen;
--- index.ios.js
+++ index.ios.js
@@ -7,82 +7,22 @@
 var React = require('react-native');
 var {
     AppRegistry,
-    Image,
-    ListView,
+    NavigatorIOS,
     StyleSheet,
-    Text,
-    View,
     } = React;
 
-var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
-var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
-var PAGE_SIZE = 25;
-var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
-var REQUEST_URL = API_URL + PARAMS;
+var SearchScreen = require('./SearchScreen');
 
 var AwesomeProject = React.createClass({
-    getInitialState: function () {
-        return {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2) => row1 !== row2,
-            }),
-            loaded: false,
-        };
-    },
-
-    componentDidMount: function () {
-        this.fetchData();
-    },
-
-    fetchData: function () {
-        fetch(REQUEST_URL)
-            .then((response) => response.json())
-            .then((responseData) => {
-                this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-                    loaded: true,
-                });
-            })
-            .done();
-    },
-
     render: function () {
-        if (!this.state.loaded) {
-            return this.renderLoadingView();
-        }
-
         return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={this.renderMovie}
-                style={styles.listView}
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                    title: 'Movies',
+                    component: SearchScreen,
+                }}
             />
-        );
-    },
-
-    renderLoadingView: function () {
-        return (
-            <View style={styles.container}>
-                <Text>
-                Loading movies...
-                </Text>
-            </View>
-        );
-    },
-
-    renderMovie: function (movie) {
-        console.log("test")
-        return (
-            <View style={styles.container}>
-                <Image
-                    source={{uri: movie.posters.thumbnail}}
-                    style={styles.thumbnail}
-                />
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{movie.title}</Text>
-                    <Text style={styles.year}>{movie.year}</Text>
-                </View>
-            </View>
         );
     },
 });
@@ -90,30 +30,7 @@ var AwesomeProject = React.createClass({
 var styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    rightContainer: {
-        flex: 1,
-    },
-    title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center',
-    },
-    year: {
-        padding: 8,
-        textAlign: 'center',
-    },
-    thumbnail: {
-        width: 53,
-        height: 81,
-    },
-    listView: {
-        paddingTop: 20,
-        backgroundColor: '#F5FCFF',
+        backgroundColor: 'white',
     },
 });
 
